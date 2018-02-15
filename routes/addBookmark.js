f599a2f02a7ac5246d76ca036d8cc695b8349691
RandomDentyne <randomdentyne@gmail.com>
2018-02-15 03:54:24 -0800

    fixed the null check

--- routes/addBookmark.js
+++ routes/addBookmark.js
@@ -1,7 +1,7 @@
 exports.view = function(req, res) {
     var redis= require("redis"), client = redis.createClient(process.env.REDIS_URL || 6379);
     var jobs = client.get('jobs');
-    if(jobs === null) {
+    if(jobs === false) {
 	    jobs = require("../jobs.json");
 	    client.set("jobs", jobs);
     }
--- routes/job_desc.js
+++ routes/job_desc.js
@@ -1,7 +1,7 @@
 exports.view = function(req, res) {
     var redis= require("redis"), client = redis.createClient(process.env.REDIS_URL || 6379);
     var job_desc = client.get('jobs');
-    if(job_desc === null) {
+    if(job_desc === false) {
 	    job_desc = require("../jobs.json");
 	    client.set("jobs", job_desc);
     }
