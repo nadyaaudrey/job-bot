43bba2af5fa29f90f19f9f885bb1b4a2f732700f
RandomDentyne <randomdentyne@gmail.com>
2018-02-22 16:24:01 -0800

    fixed syntax for jquery

--- public/js/login.js
+++ public/js/login.js
@@ -8,8 +8,8 @@ function initializePage() {
 
 function checkCred(e) {
         e.preventDefault();
-        var email = $('#Form-email1').value();
-        var pass = $('#Form-pass1').value();
+        var email = $('#Form-email1').val();
+        var pass = $('#Form-pass1').val();
         if(email.length == 0 || pass.length == 0) {
                 return;
         }
