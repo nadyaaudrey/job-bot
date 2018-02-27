f29e33ef8aa2f4f03c285f88e2b36547d1dfa7f6
RandomDentyne <randomdentyne@gmail.com>
2018-02-26 19:36:41 -0800

    app history works now

--- routes/appHistory.js
+++ routes/appHistory.js
@@ -1,10 +1,11 @@
 exports.view = async function(req, res) {
         var redis= require("redis"), client = redis.createClient(process.env.REDIS_URL || 6379);
+	var axios = require("axios");
 	var https = require("https");
         const {promisify} = require('util');
         const getAsync = promisify(client.get).bind(client);
         const setAsync = promisify(client.set).bind(client);
-	const getHttpAsync = promisify(https.get).bind(https);
+	const getHttpAsync = promisify(https.get);
         var logged_in = false;
         var user = req.cookies.user;
         if(user != undefined) {
@@ -21,8 +22,13 @@ exports.view = async function(req, res) {
 	var uinfo = await getAsync(user);
 	var uApps = JSON.parse(uinfo).applications;
 	var jobs = [];
+	var promises = [];
+	var statuses = {};
 	for(app in uApps) {
-            https.get('https://jobs.github.com/positions/' + uApps[app].id + '.json', (resp) => {
+	    console.log(app);
+	    promises.push(axios('https://jobs.github.com/positions/' + uApps[app].id + '.json'));
+	    statuses[uApps[app].id] = uApps[app].status;
+            /*await getHttpAsync('https://jobs.github.com/positions/' + uApps[app].id + '.json', (resp) => {
 	    let data = "";
 	    
 	    resp.on('data', (chunk) => {
@@ -33,14 +39,22 @@ exports.view = async function(req, res) {
 	        var job = JSON.parse(data);
 		job.status = uApps[app].status;
 		jobs.push(job);
-		//console.log(jobs);
+		console.log(jobs);
 	    });
-            }).on("error", (err) => {
-	        console.log("Error: " + err.message);
             });
+	    console.log("test"); */
 	}
-	console.log(jobs);
-	res.render('appHistory', {jobs});
+	await Promise.all(promises).then(function(values) {
+		for(value in values) {
+			console.log(value);
+			jobs.push(values[value].data);
+			jobs[value].status = statuses[jobs[value].id];
+		}
+		console.log(jobs);
+		res.render('appHistory', {jobs});
+	});
+	//console.log(jobs);
+	//res.render('appHistory', {jobs});
     
     //var jobs = require("../jobs.json");
     //res.render('appHistory', {jobs});
